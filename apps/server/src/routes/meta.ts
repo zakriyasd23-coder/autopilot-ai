91829eb2e1809dadaf98dec496aa163a6e0667a2
Muhammad Yousaf <zakriyasd23@gmail.com>
2026-05-20 12:26:15 +0500

    restore working facebook

--- apps/server/src/routes/meta.ts
+++ apps/server/src/routes/meta.ts
@@ -9,7 +9,7 @@ router.get("/login", (req, res) => {
     `https://www.facebook.com/v19.0/dialog/oauth` +
     `?client_id=${process.env.META_APP_ID}` +
     `&redirect_uri=${encodeURIComponent(process.env.META_REDIRECT_URI!)}` +
-    `&scope=public_profile,pages_show_list,pages_read_engagement,instagram_basic,business_management`;
+    `&scope=public_profile`;
 
   res.redirect(url);
 });
@@ -18,7 +18,6 @@ router.get("/callback", async (req, res) => {
   try {
     const { code } = req.query;
 
-    // exchange code
     const tokenRes = await axios.get(
       "https://graph.facebook.com/v19.0/oauth/access_token",
       {
@@ -33,7 +32,6 @@ router.get("/callback", async (req, res) => {
 
     const accessToken = tokenRes.data.access_token;
 
-    // facebook user
     const userRes = await axios.get(
       "https://graph.facebook.com/me",
       {
@@ -65,78 +63,11 @@ router.get("/callback", async (req, res) => {
       }
     );
 
-    // get pages
-    const pagesRes = await axios.get(
-      "https://graph.facebook.com/me/accounts",
-      {
-        params: {
-          access_token: accessToken,
-        },
-      }
-    );
-
-    const pages = pagesRes.data.data || [];
-
-    for (const page of pages) {
-      try {
-        const igRes = await axios.get(
-          `https://graph.facebook.com/${page.id}`,
-          {
-            params: {
-              fields: "instagram_business_account",
-              access_token: accessToken,
-            },
-          }
-        );
-
-        const igAccountId = igRes.data.instagram_business_account?.id;
-
-        if (igAccountId) {
-          const igProfileRes = await axios.get(
-            `https://graph.facebook.com/${igAccountId}`,
-            {
-              params: {
-                fields: "id,username,profile_picture_url",
-                access_token: accessToken,
-              },
-            }
-          );
-
-          const ig = igProfileRes.data;
-
-          await Account.findOneAndUpdate(
-            {
-              platform: "Instagram",
-              accountId: ig.id,
-            },
-            {
-              platform: "Instagram",
-              accountId: ig.id,
-              accountName: ig.username,
-              avatar: ig.profile_picture_url || "",
-              accessToken,
-              connected: true,
-            },
-            {
-              upsert: true,
-              new: true,
-            }
-          );
-        }
-      } catch (err) {
-        console.log("No IG linked to page");
-      }
-    }
-
     res.redirect("http://localhost:5179/dashboard");
 
   } catch (error: any) {
-    console.error(
-      "META ERROR:",
-      error.response?.data || error.message
-    );
-
-    res.send("Meta login failed");
+    console.error("FACEBOOK ERROR:", error.response?.data || error.message);
+    res.send("Facebook login failed");
   }
 });
 
